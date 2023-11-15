14a2f3b53173cf25de1a423224bfc2afe553ad59
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-11-15 12:15:31 +0530

    Create Oddoreven.java

---
+++ Oddoreven.java
@@ -0,0 +1,10 @@
+class Oddoreven_48 {
+	public static void main(String ags[]) {
+		int a=67;
+		System.out.println("The number is "+a);
+		if(a%2==0) 
+			System.out.println("Number is even");
+		else
+			System.out.println("Number is odd");
+	}
+}
