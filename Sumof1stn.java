5f34ddf6c6d51616183c1d3e0a589a59bae138de
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-11-15 12:19:57 +0530

    Create Sumof1stn.java

---
+++ Sumof1stn.java
@@ -0,0 +1,9 @@
+class SumOf1stn_48 {
+	public static void main (String args[]) {
+		int n=5,i,sum=0;
+		System.out.println("Number is "+n);
+		for(i=1;i<=n;++i)
+			sum+=i;
+		System.out.println("Sum upto "+n+"="+sum);
+	}
+}
