5a5faa9867f03a3ed409b72fe4e393db07e6a777
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2023-11-15 12:21:10 +0530

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,23 @@
+class Day_48 {
+	public static void main (String args[]) {
+		int day=5;
+		System.out.println("Entered number is "+day);
+		switch(day) {
+			case 1: System.out.println("Sunday");
+					break;
+			case 2: System.out.println("Monday");
+					break;
+			case 3: System.out.println("Tuesday");
+					break;
+			case 4: System.out.println("Wednesday");
+					break;
+			case 5: System.out.println("Thursday");
+					break;
+			case 6: System.out.println("Friday");
+					break;
+			case 7: System.out.println("Saturday");
+					break;
+			default:System.out.println("Wrong input");
+		}
+	}
+}
